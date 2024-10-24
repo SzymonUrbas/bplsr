ffa114d6fdb849f6d54353cd93e989a6dd8886ef
SzymonUrbas <34093432+SzymonUrbas@users.noreply.github.com>
2024-10-24 10:03:53 +0100

    make package

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Milk traits and corresponding mid-infrared spectra
+#'
+#' Data containing spectral measurements for XX milk samples originating from XX cows along with various chemical and technological traits measures.
+#' @format A list of 3 dataframes:
+#' \item{info}{}
+#'
+#'
+
+#' @references Streuli, H. (1973). Der heutige Stand der Kaffee-Chemie, In \emph{Proceedings of the 6th International Colloquium on Coffee Chemistry}, June 4-June 9, 1973, Bogata, Colombia, pp. 61-72. Paris, France: Association Scientifique International du Café (ASIC).
+#' @examples
+#' data(milk_MIR, package="BPLS")
+#' @docType data
+#' @keywords datasets
+#' @usage data(milk_MIR)
+"milk_MIR"
